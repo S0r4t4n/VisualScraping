4294206a32abf50e9913d58cd4e430dd8e11476b
S0r4t4n <sora2460@gmail.com>
2020-08-08 05:51:24 +0900

    initial commit

---
+++ target_block.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['target_url'] = {
+  init: function() {
+    this.appendValueInput("target_url")
+        .setCheck("String")
+        .appendField("target_url");
+    this.setColour(290);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
